9eb3400615c35b85e242ab7c0084869bacb9bcfd
alveek <alveek@gmail.com>
2022-12-05 10:49:45 +0300

    solve javascriptmas day 5 challenge

---
+++ javascriptmas-22/day-5_candy_sale/index.js
@@ -0,0 +1,31 @@
+import products from "./data.js";
+
+/*
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+
+let shoppingCart = [];
+
+function filterSweets(items) {
+  return items.filter(item => item.type === "sweet");
+}
+
+function getSaleItems(data){
+  shoppingCart = filterSweets(data);
+};
+
+getSaleItems(products);
+console.log(shoppingCart);
