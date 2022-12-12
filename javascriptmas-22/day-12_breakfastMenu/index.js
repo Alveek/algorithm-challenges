06abef599184a936ddb28c4be503da57c48f9fe4
alveek <alveek@gmail.com>
2022-12-12 19:23:12 +0300

    javascriptmas day 12

---
+++ javascriptmas-22/day-12_breakfastMenu/index.js
@@ -0,0 +1,30 @@
+/* Challenge 2: Breakfast Menu
+- Topic: .map()
+Our restaurant menu currently only shows the breakfast menu, 
+as it has been hard coded into the HTML file. However, we want 
+to offer a dinner menu instead. Let's fix this using .map()
+
+1. First, fetch a reference to the menu <section> from the DOM. 
+2. Set the innerHTML content of the menu <section> to...
+3. the dinnerFoods array by mapping over the array and returning 
+the following div for each food in the array: 
+`<div class="food">FOOD VALUE HERE</div>` 
+4. Remember to remove any separating commas between the food divs!
+*/
+
+const dinnerFoods = ['🍝', '🍔', '🌮'];
+const menu = document.querySelector('#menu');
+menu.innerHTML = dinnerFoods.map(item => {
+  return `<div class="food">${item}</div>`
+}).join("");
+
+/* <body>
+<h2>Menu</h2>
+<section id="menu">
+   <div class="food">🥐</div>
+   <div class="food">🥚</div>
+   <div class="food">☕️</div>
+</section>
+
+<script src="index.js"></script>
+</body> */
