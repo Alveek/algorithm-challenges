3ace3dcd408ba84f193940bed21eec2005d8f880
alveek <alveek@gmail.com>
2022-12-20 13:32:28 +0300

    javascriptmas day 19

---
+++ javascriptmas-22/day-19_centuryFromYear/index.js
@@ -0,0 +1,39 @@
+function centuryFromYear(num) {
+  const nArr = Array.from(String(num));
+  const n1 = Number(nArr.slice(0, 2).join(""));
+  const n2 = Number(nArr.slice(2, 4).join(""));
+
+  return n2 < 1 ? n1 : n1 + 1;
+}
+
+* Test Suite 
+*/
+describe('centuryFromYear()', () => {
+    it('returns current century', () => {
+        // arrange
+        const year = 1905;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(20);
+    });
+    
+    it('returns current century for edge case of start of century', () => {
+        // arrange
+        const year = 1700;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(17);
+    });    
+});
