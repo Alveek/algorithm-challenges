288f8b81d5b1e98901f99588645cb3965d7d1652
alveek <alveek@gmail.com>
2022-12-20 13:33:50 +0300

    fix: javascriptmas day 19

--- javascriptmas-22/day-19_centuryFromYear/index.js
+++ javascriptmas-22/day-19_centuryFromYear/index.js
@@ -6,34 +6,34 @@ function centuryFromYear(num) {
   return n2 < 1 ? n1 : n1 + 1;
 }
 
-* Test Suite 
-*/
-describe('centuryFromYear()', () => {
-    it('returns current century', () => {
-        // arrange
-        const year = 1905;
-        
-        // act
-        const result = centuryFromYear(year);
+// * Test Suite
+// */
+// describe('centuryFromYear()', () => {
+//     it('returns current century', () => {
+//         // arrange
+//         const year = 1905;
 
-        // log
-        console.log("result 1: ", result);
-        
-        // assert
-        expect(result).toBe(20);
-    });
-    
-    it('returns current century for edge case of start of century', () => {
-        // arrange
-        const year = 1700;
-        
-        // act
-        const result = centuryFromYear(year);
+//         // act
+//         const result = centuryFromYear(year);
 
-        // log
-        console.log("result 2: ", result);
-        
-        // assert
-        expect(result).toBe(17);
-    });    
-});
+//         // log
+//         console.log("result 1: ", result);
+
+//         // assert
+//         expect(result).toBe(20);
+//     });
+
+//     it('returns current century for edge case of start of century', () => {
+//         // arrange
+//         const year = 1700;
+
+//         // act
+//         const result = centuryFromYear(year);
+
+//         // log
+//         console.log("result 2: ", result);
+
+//         // assert
+//         expect(result).toBe(17);
+//     });
+// });
