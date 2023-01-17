4d98fe30c74cc4d951ea887ff41378f5a873feda
alveek <alveek@gmail.com>
2023-01-18 00:04:25 +0300

    leetcode 1480

--- javascriptmas-22/day-19_centuryFromYear/index.js
+++ javascriptmas-22/day-19_centuryFromYear/index.js
@@ -1,7 +1,7 @@
 function centuryFromYear(num) {
   const nArr = Array.from(String(num));
-  const n1 = Number(nArr.slice(0, 2).join(""));
-  const n2 = Number(nArr.slice(2, 4).join(""));
+  const n1 = Number(nArr.slice(0, 2).join(''));
+  const n2 = Number(nArr.slice(2, 4).join(''));
 
   return n2 < 1 ? n1 : n1 + 1;
 }
